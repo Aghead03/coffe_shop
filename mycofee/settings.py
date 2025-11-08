662730e9aa579731ef7b9e2d2b6f86fe00094153
Aghead03 <agheadhajjeh@gmail.com>
2025-11-08 16:10:50 +0300

    edit database settings

--- mycofee/settings.py
+++ mycofee/settings.py
@@ -78,13 +78,8 @@ WSGI_APPLICATION = 'mycofee.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mycoffeedb1',
-        'USER': 'postgres',
-        'PASSWORD': 'aghead',
-        'HOST': 'localhost',
-        'PORT': '5432',
-        
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
